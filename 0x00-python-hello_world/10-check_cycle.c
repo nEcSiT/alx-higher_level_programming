7ce626c3d436d116ca25bcd19378e6b04a51c32b
nEcSiT <nicholasdornyo11@gmail.com>
2023-10-30 20:36:01 +0000

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /*
  * check_cycle - checks if a linked list
  * @list: linked list to check
@@ -8,28 +7,27 @@
  */
 
 int check_cycle(listint_t *list)
-{	
-	
-
-	listint_t *cpt  = list;
-      	listint_t *cpt1 = list;
-
-	if (list == NULL)
-		return (0);
+{
 
 
-        while (cpt  && cpt1  && cpt1->next)
-	{
+listint_t *cpt  = list;
+listint_t *cpt1 = list;
 
-                cpt = cpt->next;
-		cpt1  = cpt->next->next;
-
-                if (cpt  == cpt1)
-			return (1);
-
-        }
+if (list == NULL)
+return (0);
 
 
-        return (0);
+while (cpt  && cpt1  && cpt1->next)
+{
+
+cpt = cpt->next;
+cpt1  = cpt->next->next;
+
+if (cpt  == cpt1)
+return (1);
+
+}
+
+return (0);
 
 }
