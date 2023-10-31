e46aa04331fce0a6d49cf6bdf3d6ffcfb568f477
nEcSiT <nicholasdornyo11@gmail.com>
2023-10-31 23:48:07 +0000

    isNegOrPos

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,16 +3,16 @@ import random
 number = random.randint(-10, 10)
 
 # Cheecking the codition if the number is positive
-if number > 0:
-    print(f'{number} is positive')
+    if number > 0:
+         print(f'{number} is positive')
 
 # Cheecking the codition if the number is 0
-elif number == 0:
-     print(f'{number} is zero')
+    elif number == 0:
+         print(f'{number} is zero')
 
 # Cheecking the codition if the number is negative
-else:
-     print(f'{number} is negative')
+    else:
+         print(f'{number} is negative')
 
 # Printing a newline
 print()
