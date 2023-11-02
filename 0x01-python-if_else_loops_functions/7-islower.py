417571349b1fba10f80ca00ff619028b482a126f
nEcSiT <nicholasdornyo11@gmail.com>
2023-11-02 01:31:37 +0000

    lo_up

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+
+    """Code to check for upper or lower case number"""
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
