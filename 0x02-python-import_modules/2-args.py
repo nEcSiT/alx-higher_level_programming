576974094eea6ad1c7595f668e0289ce9d9f9a64
nEcSiT <nicholasdornyo11@gmail.com>
2023-11-06 15:55:37 +0000

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    import sys
+
+    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name itself
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        if num_args == 1:
+            print("1 argument:")
+        else:
+            print(f"{num_args} arguments:")
+
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
