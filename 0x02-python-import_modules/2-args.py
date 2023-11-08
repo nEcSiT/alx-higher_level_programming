d84485af513003e4eaf81662c170bf930a1a29f8
nEcSiT <nicholasdornyo11@gmail.com>
2023-11-08 10:56:35 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Prints the result of the addition of all arguments"""
     import sys
-    length  = len(sys.argv)
 
-    if length == 1:
-        print("{:d} arguments.".format(length - 1))
-    elif length == 2:
-        print("{:d} argument:".format(length - 1))
+    count = len(sys.argv) - 1
+
+    # Convert arguments to integers and calculate the sum
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{:d} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
